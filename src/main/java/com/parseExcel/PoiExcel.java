c7c29cf829cdb0ffa3153b4466a04b43dd9e6f67
hhzh <1138253815@qq.com>
2017-07-24 14:12:45 +0800

    update

--- src/main/java/com/parseExcel/PoiExcel.java
+++ src/main/java/com/parseExcel/PoiExcel.java
@@ -15,116 +15,116 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class PoiExcel {
-	public static void main(String[] args) {
-//		writeExcel();
-//		writeExcel2();
-//		readExcel();
-	}
-	
-	public static void writeExcel(){
-		String[] title = {"id","name","sex"};
-		
-		//����Excel������
-		HSSFWorkbook workbook = new HSSFWorkbook();
-		//����һ��������sheet
-		HSSFSheet sheet = workbook.createSheet();
-		//������һ��
-		HSSFRow row = sheet.createRow(0);
-		HSSFCell cell = null;
-		//�����һ������ id,name,sex
-		for (int i = 0; i < title.length; i++) {
-			cell = row.createCell(i);
-			cell.setCellValue(title[i]);
-		}
-		//׷������
-		for (int i = 1; i <= 10; i++) {
-			HSSFRow nextrow = sheet.createRow(i);
-			HSSFCell cell2 = nextrow.createCell(0);
-			cell2.setCellValue("a" + i);
-			cell2 = nextrow.createCell(1);
-			cell2.setCellValue("user" + i);
-			cell2 = nextrow.createCell(2);
-			cell2.setCellValue("��");
-		}
-		//����һ���ļ�
-		File file = new File("e:/poi_test.xls");
-		try {
-			file.createNewFile();
-			//��Excel���ݴ���
-			FileOutputStream stream = FileUtils.openOutputStream(file);
-			workbook.write(stream);
-			stream.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void writeExcel2(){
-		String[] title = {"id","name","sex"};
-		
-		//����Excel������
-		XSSFWorkbook workbook = new XSSFWorkbook();
-		//����һ��������sheet
-		Sheet sheet = workbook.createSheet();
-		//������һ��
-		Row row = sheet.createRow(0);
-		Cell cell = null;
-		//�����һ������ id,name,sex
-		for (int i = 0; i < title.length; i++) {
-			cell = row.createCell(i);
-			cell.setCellValue(title[i]);
-		}
-		//׷������
-		for (int i = 1; i <= 10; i++) {
-			Row nextrow = sheet.createRow(i);
-			Cell cell2 = nextrow.createCell(0);
-			cell2.setCellValue("a" + i);
-			cell2 = nextrow.createCell(1);
-			cell2.setCellValue("user" + i);
-			cell2 = nextrow.createCell(2);
-			cell2.setCellValue("��");
-		}
-		//����һ���ļ�
-		File file = new File("e:/poi_test2.xlsx");
-		try {
-			file.createNewFile();
-			//��Excel���ݴ���
-			FileOutputStream stream = FileUtils.openOutputStream(file);
-			workbook.write(stream);
-			stream.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void readExcel(){
-		// ��Ҫ������Excel�ļ�
-		File file = new File("e:/poi_test.xls");
-		try {
-			// ����Excel����ȡ�ļ�����
-			HSSFWorkbook workbook = new HSSFWorkbook(
-					FileUtils.openInputStream(file));
-			// ��ȡ��һ��������workbook.getSheet("Sheet0");
-			// HSSFSheet sheet = workbook.getSheet("Sheet0");
-			// ��ȡĬ�ϵ�һ��������sheet
-			HSSFSheet sheet = workbook.getSheetAt(0);
-			int firstRowNum = 0;
-			// ��ȡsheet�����һ���к�
-			int lastRowNum = sheet.getLastRowNum();
-			for (int i = firstRowNum; i <= lastRowNum; i++) {
-				HSSFRow row = sheet.getRow(i);
-				// ��ȡ��ǰ�����Ԫ���к�
-				int lastCellNum = row.getLastCellNum();
-				for (int j = 0; j < lastCellNum; j++) {
-					HSSFCell cell = row.getCell(j);
-					String value = cell.getStringCellValue();
-					System.out.print(value + "  ");
-				}
-				System.out.println();
-			}
+    public static void main(String[] args) {
+        //writeExcel();
+        //writeExcel2();
+        readExcel();
+    }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void writeExcel() {
+        String[] title = {"id", "name", "sex"};
+
+        //创建Excel工作簿
+        HSSFWorkbook workbook = new HSSFWorkbook();
+
+        //创建一个工作表sheet
+        HSSFSheet sheet = workbook.createSheet();
+        //创建第一行
+        HSSFRow row = sheet.createRow(0);
+        HSSFCell cell;
+        //插入第一行数据 id,name,sex
+        for (int i = 0; i < title.length; i++) {
+            cell = row.createCell(i);
+            cell.setCellValue(title[i]);
+        }
+        //追加数据
+        for (int i = 1; i <= 10; i++) {
+            HSSFRow nextRow = sheet.createRow(i);
+            cell = nextRow.createCell(0);
+            cell.setCellValue("a" + i);
+            cell = nextRow.createCell(1);
+            cell.setCellValue("user" + i);
+            cell = nextRow.createCell(2);
+            cell.setCellValue("男");
+        }
+        //创建一个文件
+        File file = new File("e:/poi_test.xls");
+        try {
+            file.createNewFile();
+            //将Excel内容存盘
+            FileOutputStream stream = FileUtils.openOutputStream(file);
+            workbook.write(stream);
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeExcel2() {
+        String[] title = {"id", "name", "sex"};
+
+        //创建Excel工作簿
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        //创建一个工作表sheet
+        Sheet sheet = workbook.createSheet();
+        //创建第一行
+        Row row = sheet.createRow(0);
+        Cell cell = null;
+        //插入第一行数据 id,name,sex
+        for (int i = 0; i < title.length; i++) {
+            cell = row.createCell(i);
+            cell.setCellValue(title[i]);
+        }
+        //追加数据
+        for (int i = 1; i <= 10; i++) {
+            Row nextRow = sheet.createRow(i);
+            cell = nextRow.createCell(0);
+            cell.setCellValue("a" + i);
+            cell = nextRow.createCell(1);
+            cell.setCellValue("user" + i);
+            cell = nextRow.createCell(2);
+            cell.setCellValue("男");
+        }
+        //创建一个文件
+        File file = new File("e:/poi_test2.xlsx");
+        try {
+            file.createNewFile();
+            //将Excel内容存盘
+            FileOutputStream stream = FileUtils.openOutputStream(file);
+            workbook.write(stream);
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readExcel() {
+        //需要解析的Excel文件
+        File file = new File("e:/poi_test.xls");
+        try {
+            //创建Excel，读取文件内容
+            HSSFWorkbook workbook = new HSSFWorkbook(FileUtils.openInputStream(file));
+            //获取第一个工作表workbook.getSheet("Sheet0");
+//			HSSFSheet sheet = workbook.getSheet("Sheet0");
+            //读取默认第一个工作表sheet
+            HSSFSheet sheet = workbook.getSheetAt(0);
+            int firstRowNum = 0;
+            //获取sheet中最后一行行号
+            int lastRowNum = sheet.getLastRowNum();
+            for (int i = firstRowNum; i <= lastRowNum; i++) {
+                HSSFRow row = sheet.getRow(i);
+                //获取当前行最后单元格列号
+                int lastCellNum = row.getLastCellNum();
+                for (int j = 0; j < lastCellNum; j++) {
+                    HSSFCell cell = row.getCell(j);
+                    String value = cell.getStringCellValue();
+                    System.out.print(value + "\t");
+                }
+                System.out.println();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
