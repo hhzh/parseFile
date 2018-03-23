4278bc446cc11b6bd2d14b2e9881efedca2ca3b7
hhzh <1138253815@qq.com>
2018-03-23 19:42:16 +0800

    add

--- src/main/java/com/test/Demo.java
+++ src/main/java/com/test/Demo.java
@@ -5,6 +5,24 @@ import java.util.List;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println(Runtime.getRuntime().availableProcessors());
+        //List<User> list = new ArrayList<User>();
+        //User user = new User();
+        //user.setName("zhangsan");
+        //list.add(user);
+        //list.add(null);
+        //User user1 = new User();
+        //user1.setName("lisi");
+        //list.add(user1);
+        //list.add(null);
+        //
+        //for (User user2 : list) {
+        //    System.out.println(user2);
+        //}
+
+        //System.out.println(Runtime.getRuntime().availableProcessors());
+
+        for (int i = 1; i < 10001; i++) {
+            System.out.println("D:/imgtest/img/" + i + ".jpg");
+        }
     }
 }
