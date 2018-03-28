f5a32d5281a9494fe5b469110d21ad32bc3f5d49
hhzh <1138253815@qq.com>
2018-03-28 18:58:58 +0800

    add

--- src/main/java/com/test/Demo.java
+++ src/main/java/com/test/Demo.java
@@ -24,7 +24,21 @@ public class Demo {
         //for (int i = 1; i < 10001; i++) {
         //    System.out.println("D:/imgtest/img/" + i + ".jpg");
         //}
-        String name = "cropImg2.jpg";
-        System.out.println(name.substring(name.lastIndexOf(".") + 1));
+        //String name = "cropImg2.jpg";
+        //System.out.println(name.substring(name.lastIndexOf(".") + 1));
+        //String ab = "ab";
+        //String a = "a";
+        //String b = "b";
+        //String abObj = new String("ab");
+        //String abbb = a + b;
+        //System.out.println(ab.equals(abbb));
+        //System.out.println(ab.equals(abObj));
+        //System.out.println(ab==abbb);
+        //System.out.println(ab==abObj);
+        //Object obj = new Object();
+
+        String groupPath = "group1/M00/01/3A/cnBodVq5wq-AYi7yACttOcwyD8c864.jpg";
+        System.out.println(groupPath.substring(0, groupPath.indexOf("/")));
+        System.out.println(groupPath.substring(groupPath.indexOf("/") + 1));
     }
 }
