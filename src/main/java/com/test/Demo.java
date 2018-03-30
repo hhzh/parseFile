3ce978b434c23d76da75e1ee9250190c843a33e7
hhzh <1138253815@qq.com>
2018-03-30 18:35:41 +0800

    add

--- src/main/java/com/test/Demo.java
+++ src/main/java/com/test/Demo.java
@@ -1,6 +1,12 @@
 package com.test;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjuster;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Demo {
@@ -37,8 +43,29 @@ public class Demo {
         //System.out.println(ab==abObj);
         //Object obj = new Object();
 
-        String groupPath = "group1/M00/01/3A/cnBodVq5wq-AYi7yACttOcwyD8c864.jpg";
-        System.out.println(groupPath.substring(0, groupPath.indexOf("/")));
-        System.out.println(groupPath.substring(groupPath.indexOf("/") + 1));
+        //String groupPath = "group1/M00/01/3A/cnBodVq5wq-AYi7yACttOcwyD8c864.jpg";
+        //System.out.println(groupPath.substring(0, groupPath.indexOf("/")));
+        //System.out.println(groupPath.substring(groupPath.indexOf("/") + 1));
+
+        //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //Date date = new Date();
+        //System.out.println(date);
+        //System.out.println(sdf.format(date));
+
+        //LocalDate now = LocalDate.now();
+        //System.out.println(now);
+        //LocalDateTime now1 = LocalDateTime.now();
+        //DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("YYYY-MM-DD HH:mm:ss");
+        //System.out.println(now1);
+        //String format = dateTimeFormatter.format(now1);
+        //System.out.println(format);
+        //LocalDateTime localDateTime = now1.minusMinutes(3);
+        //System.out.println(localDateTime);
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime localDateTime = now.minusMinutes(3);
+        String formatDate = dateTimeFormatter.format(localDateTime);
+        System.out.println(formatDate);
     }
 }
